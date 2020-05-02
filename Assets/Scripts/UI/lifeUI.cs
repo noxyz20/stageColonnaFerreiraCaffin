303a3bd6a5a65f2c8797764b95cc9edc3c1b7c14
Franck Colonna <noxyzdisign@gmail.com>
2020-05-02 16:46:44 +0200

    life ui

---
+++ Assets/Scripts/UI/lifeUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Platformer.Mechanics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lifeUI : MonoBehaviour
+{
+    public Sprite[] HearthSprites;
+    public Image HearthUI;
+    private Health _player;
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+    void Update()
+    {
+        HearthUI.sprite = HearthSprites[_player.currentHP];
+    }
+}
