80b803d1b81f4962ae9238f5e1b6ad60f43d881b
Franck Colonna <noxyzdisign@gmail.com>
2020-05-07 23:54:38 +0200

    gate lvl2 + loop

--- Assets/Scripts/GateLevel2.cs
+++ Assets/Scripts/GateLevel2.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GateLevel1 : MonoBehaviour
+public class GateLevel2 : MonoBehaviour
 {
     void OnTriggerEnter2D()
     {
