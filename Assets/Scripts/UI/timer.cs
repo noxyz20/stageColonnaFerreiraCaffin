6a88c3051b7b787a13e1e2039802a2dbfac43857
Vodkatypique <clement.caffin@gmail.com>
2020-05-23 18:48:55 +0200

    debut menage

--- Assets/Scripts/UI/timer.cs
+++ Assets/Scripts/UI/timer.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class timer : MonoBehaviour
@@ -10,11 +11,27 @@ public class timer : MonoBehaviour
     float theTimer = 0.0f;
     public float theStartTime = 120.0f;
     public bool showRemaining;
+
+    private int lastScore;
     // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        lastScore  =  PlayerPrefs.GetInt("score");
+        print("SCORE : "+ lastScore);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetInt("score", Convert.ToInt32(10*ecoule));
+        print("SCORE Save : "+ Convert.ToInt32(10*ecoule));
+    }
+
     void Start()
     {
         showRemaining = true;
         theTimer = theStartTime;
+        int lastScore  =  PlayerPrefs.GetInt("score");
     }
 
     // Update is called once per frame
@@ -24,7 +41,7 @@ public class timer : MonoBehaviour
         theTimer -= Time.deltaTime;
         ecoule += Time.deltaTime;
         //print(theTimer);
-        if (theTimer <= 0) 
+        if (theTimer <= 0)
         {
             Debug.Log("OUT OF TIME");
             theTimer = 0;
@@ -35,10 +52,12 @@ public class timer : MonoBehaviour
     
     void OnGUI() 
     {
-        string text = string.Format( "{0:00}:{1:00}", Convert.ToInt32( theTimer / 60.0 ), Convert.ToInt32( theTimer % 60.0 ) ); 
+        string text = string.Format( "Encore {0:00}:{1:00}", Convert.ToInt32( theTimer / 60.0 ), Convert.ToInt32( theTimer % 60.0 ) );
+        string score = string.Format("Score : {0}", lastScore + Convert.ToInt32(10*ecoule));
         if (showRemaining)
         {
-            GUI.Label( new Rect(600, 20, Screen.width - 20, 30), text );
+            GUI.Label( new Rect(0, 30, Screen.width - 20, 30), text );
+            GUI.Label( new Rect(0, 40, Screen.width - 20, 30), score );
         }
     }
 }
