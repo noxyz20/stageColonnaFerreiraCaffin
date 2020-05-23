a38b56a15c7220b2d0b15accec6c19fc49b54a6c
Vodkatypique <clement.caffin@gmail.com>
2020-05-23 18:48:55 +0200

    debut menage

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,10 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        try
+        {
+            if (PlayerPrefs.GetInt("score") > int.Parse(File.ReadAllText("score.scoreFile")))
+            {
+                File.WriteAllText("score.scoreFile", PlayerPrefs.GetInt("score").ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            File.WriteAllText("score.scoreFile", PlayerPrefs.GetInt("score").ToString());
+        }
+        
+    }
     public void BackMenu()
     {
         SceneManager.LoadScene("Main Menu");
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    private void OnGUI()
+    {
+        GUI.Label( new Rect(250, 25, Screen.width - 20, 30), File.ReadAllText("score.scoreFile"));
+    }
+
+
     public void Play()
     {
+        PlayerPrefs.SetInt("score", 1);
         SceneManager.LoadScene("level1");
     }
 
